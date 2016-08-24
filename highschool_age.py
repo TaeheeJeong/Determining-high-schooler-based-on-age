4dd297ebbcd6d851b0dc08d26a24107ca2935a29
TaeheeJeong <taehee77@gmail.com>
2016-08-24 16:19:12 -0700

    Add files via upload

---
+++ highschool_age.py
@@ -0,0 +1,15 @@
+# Determining high schooler based on age
+"""
+Created on Aug 24 2016
+
+@author: Taehee Jeong
+"""
+
+
+def is_highschool(age):
+    if age >=14 and age <=18:
+        print "High school student"
+    else:
+        print "Not High school student"
+        
+        
